7cf74b5b8f0b439fa19e47400f630dc369be27d4
Muhammad Nurul Afif Maliki <afif.maliki21@gmail.com>
2023-07-02 22:18:08 +0700

    setting up client folder

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 mongoose
   .connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
